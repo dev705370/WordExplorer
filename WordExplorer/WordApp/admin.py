d6e711c0d465f80986d79881ec4927088c40dbe7
Devendra Kumar Mittal <dev705370@gmail.com>
2015-09-21 20:26:20 +0530

    Admin

--- WordExplorer/WordApp/admin.py
+++ WordExplorer/WordApp/admin.py
@@ -3,53 +3,27 @@ from WordApp.models import Word, Root, WordRoot, Adjective, Noun
 from autocomplete_light import shortcuts
 
 
-class RootAdmin(admin.ModelAdmin) :
+class Root_WordAdmin(admin.ModelAdmin) :
     fieldsets = [
         ('Root', {'fields': ['word', 'meaning']}),
-        ('Comment', {'fields' : ['comment'], 'classes': ['collapse']}),
-        ]
-
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-
-class MyApp1Admin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-    inlines = [ChoiceInline]
-    list_display = ('question', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['question']
-    date_hierarchy = 'pub_date'
-
-class RootWordAdmin(admin.ModelAdmin) :
-    fieldsets = [
-        ('Root Word', {'fields': ['word', 'meaning']}),
         ('Comment', {'fields' : ['comment'], 'classes': ['collapse']}),
         ]
     list_display = ('word', 'meaning', 'comment')
     list_filter = ['word']
     search_fields = ['word']
 
-class AutoCompleteAdmin(admin.ModelAdmin):
-    form = shortcuts.modelform_factory(Word_Root_Relationship, exclude=['word_id'])
+admin.site.register(Root, Root_WordAdmin)
+admin.site.register(Word, Root_WordAdmin)
 
-class WordAdmin(admin.ModelAdmin) :
-    fieldsets = [
-        ('Word', {'fields': ['word', 'meaning']}),
-        ('Comment', {'fields': ['comment'], 'classes': ['collapse']}),
-        ]
-#class RootWOrdAutoComplete(autocomplete_light.AutocompleteModelBase):
-#    search_fields = ['^word']
-#    model = RootWord
+class WordRootAdmin(admin.ModelAdmin):
+    form = shortcuts.modelform_factory(WordRoot, exclude=[])
+    extra = 5
+admin.site.register(WordRoot, WordRootAdmin)
 
-#admin.site.register(Word_Root_Relationship, AutoCompleteAdmin)
+class NounAdmin(admin.ModelAdmin):
+    form = shortcuts.modelform_factory(Noun, exclude=[])
+admin.site.register(Noun, NounAdmin)
 
-#autocomplete_light.register(RootWOrdAutoComplete)
-
-#admin.site.register(MyApp1, MyApp1Admin)
-admin.site.register(RootWord, RootWordAdmin)
-admin.site.register(Word, WordAdmin)
+class AdjectiveAdmin(admin.ModelAdmin):
+    form = shortcuts.modelform_factory(Adjective, exclude = [])
+admin.site.register(Adjective, AdjectiveAdmin)
